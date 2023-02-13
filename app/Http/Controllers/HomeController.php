f1813d208f6f57d1b4352965d89592e182ed1b91
emirothman22 <emirothman22@gmail.com>
2023-02-13 17:56:38 +0700

    middleware dashboard & integration admin dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,13 +10,15 @@ class HomeController extends Controller
 {
     public function dashboard() {
 
-        $checkouts = Checkout::with('Camp')->whereUserId(Auth::id())->get();
-        // sama aja dengan
-        // $checkouts = Checkout::with('Camp')->where('user_id', Auth::id())->get();
-
-        return view('user.dashboard', [
-            'checkouts' => $checkouts
-        ]);
+        switch (Auth::user()->is_admin) {
+            case true:
+                return redirect(route('admin.dashboard'));
+                break;
+            
+            default:
+                return redirect(route('user.dashboard'));
+                break;
+        }
     }
     //
 }
---
+++ app/Http/Controllers/User/DashboardController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Checkout;
+use Auth;
+
+class DashboardController extends Controller
+{
+
+    public function index()
+    {
+
+        $checkout = Checkout::with('Camp')->whereUserId(Auth::id())->get();
+        return view('user.dashboard', [
+            'checkouts'  => $checkout
+        ]);
+        # code...
+    }
+    //
+}
